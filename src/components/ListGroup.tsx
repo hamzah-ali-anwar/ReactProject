eba69d725fcf7a56cbb1d92cd7f1097f7970c7a6
Hamzah <hamzalianwar@hotmail.com>
2025-02-03 16:00:14 -0500

    React project first draft

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,32 @@
+import { MouseEvent, useState } from "react";
+function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let selectedIndex = 0;
+  useState;
+  // Event handling
+  const handleClick = (event: MouseEvent) => console.log(event);
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
